48ce606e87469349274f796dcf48c500ed97056e
Brian Brietzke <bbrietzke+github@gmail.com>
2022-08-22 12:31:58 -0500

    Fucking modules.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod operator; 
+mod utils;
+mod cloudflare;
+
+use crate::operator::start_operator;
+
+#[macro_use] extern crate log;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let matches = utils::init();
+
+    let zone_id = matches.value_of("zone-id").unwrap();
+    let api_token = matches.value_of("api-token").unwrap();
+
+
+    println!("{:?}", zone_id);
+    println!("{:?}", api_token);
+
+    match start_operator(api_token.to_string(), zone_id.to_string()).await {
+        Err(e) => panic!("{:?}", e),
+        _ => print!("We're okay!")
+    }
 }
