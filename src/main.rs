18fd5d318f6b58e2a4e58d85524ff9134d0a33b4
Brian Brietzke <bbrietzke+github@gmail.com>
2022-08-23 16:30:40 -0500

    Basic wiring works.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use operator::start_operations;
+
 mod utils;
+mod operator;
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::init();
 
     let matches = utils::args::init();
@@ -9,4 +12,6 @@ fn main() {
     let ip_address = matches.value_of("ip").expect("must provide an ip address");
     let zone_id = matches.value_of("zone-id").expect("must provide a zone id");
     let api_token = matches.value_of("api-token").expect("must provide api token");
+
+    start_operations(api_token, zone_id, ip_address).await;
 }
